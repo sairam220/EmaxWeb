9467e54bcaba094a74d2011da4d6384a118cd9f9
sairam220 <gudidasairam123@gmail.com>
2025-03-20 00:43:45 +0530

    emx web chnages

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+// routes.js - Define route paths as objects for sitemap generation
+
+const routes = [
+    { path: '/' },
+    { path: '/home' },
+    { path: '/services' },
+    { path: '/industries' },
+    { path: '/contact' },
+    { path: '/about-us' },
+    { path: '/trends-advances' },
+    { path: '/specializedProducts' }
+  ];
+  
+  export default routes;
+  
